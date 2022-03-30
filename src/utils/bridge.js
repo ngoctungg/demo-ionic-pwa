d52c6ddf0ae966ff1f3afc138c1030c369f81b1c
tungmn <ngoctung.mai@vmgmedia.vn>
2022-03-30 10:08:59 +0700

    asdf

--- src/utils/bridge.js
+++ src/utils/bridge.js
@@ -1,7 +1,7 @@
+/* eslint-disable */
 export default {
   callNative(data) {
     //android
-    const Android = window.Android;
     if (typeof Android !== "undefined" && Android !== null) {
         Android.nvShowToast(data);
     } else {
