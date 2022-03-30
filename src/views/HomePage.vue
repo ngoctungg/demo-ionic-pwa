b56ab2656210b16d28cb55253937ca7ab11fe6e5
tungmn <ngoctung.mai@vmgmedia.vn>
2022-03-30 10:35:16 +0700

    d

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -41,7 +41,7 @@
           @didDismiss="setOpen(false)"
         >
         </ion-toast>
-        <input type="file" id="my_file" @change="onFileChange" name="file" accept="image/*"/>
+        <input type="file" id="my_file" @change="onFileChange" name="file" accept="image/*" capture/>
         <img v-if="url" :src="url" />
         <ion-item>
           <ion-label position="fixed">Fixed Label</ion-label>
